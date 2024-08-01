83c345e6a68d349713ac4d1d58cba9c16fee7c6b
Noah Miller <106935118+NoahIsaacMiller@users.noreply.github.com>
2024-08-01 13:49:16 +0800

    Add files via upload
    更新一手

--- pic2text.py
+++ pic2text.py
@@ -8,13 +8,13 @@
 """
 
 
-# 保Pillow,Click导入
+# 保Pillow导入
 import os
 try:
     from PIL import Image, ImageDraw, ImageFont
     import click
 except:
-    print("缺失Pillow库, 正在安装(但一定要有Pip)...")
+    print("缺失关键库, 正在安装(但一定要有Pip)...")
     os.system("'pip install pillow click  -i https://pypi.mirrors.ustc.edu.cn/simple/")
     print("安转完成")
     from PIL import Image, ImageDraw, ImageFont
@@ -24,15 +24,15 @@ except:
 # 参数设置
 class Setting:
     chunkHeight = chunkWidth = 16               # 拼图块尺寸
-    fontSize = 18                               # 字体大小
-    imageMode = "RGBA"                           # 图像模式
+    fontSize = 22                               # 字体大小
+    imageMode = "RGBA"                          # 图像模式
 
     fontRelPath = "./AliPuHui-Bold.ttf"         # 字体文件相对路径
     f = open(fontRelPath, "rb")                 # 下面连着三步是为了能正常创建字体对象, 如果这样依旧报错, 把字体路径改成绝对路径
     font = ImageFont.truetype(f)                
     f.close()
 
-    chunkFill = "lightgrey"                     # 拼图块背景填充颜色, 亮灰色效果可以
+    chunkFill = "grey"                     # 拼图块背景填充颜色, 亮灰色效果可以
     chunkOutline = None                         # 拼图块边框, 一般测试的时候用, 可以把拼图框架网格化
     chunkOutlineWidth = 1                       # 拼图块边框宽度
 
@@ -40,11 +40,11 @@ setting = Setting()
 
 
 # 用(已经绑定拼图块图片对象的)ImageDraw对象绘制拼图块
-def drawChar2ChunkByImageDrawObj(char, draw:ImageDraw.ImageDraw, offsetX:int, offsetY:int, fill):
+def drawChar2ChunkByImageDrawObj(char, draw:ImageDraw.ImageDraw, offsetX:int, offsetY:int, fill, outline, color):
     draw.rectangle(
         (0, 0, setting.chunkWidth, setting.chunkHeight),
-        fill=setting.chunkFill,
-        outline=setting.chunkOutline,
+        fill=color,
+        outline=outline,
         width=setting.chunkOutlineWidth,
     )
     # 渲染文字, anchor特别重要,它指定了对其方式|anchor文档传送门: https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html#text-anchors  
@@ -63,25 +63,38 @@ def drawChar2ChunkByImageDrawObj(char, draw:ImageDraw.ImageDraw, offsetX:int, of
 @click.command()
 @click.argument("fp",type=click.Path(exists=True))
 @click.argument("text")
-@click.option("-o", "--output",default="output.png")
-def process(text, fp, output="output.png"):
+@click.option("-c", "--color", default=setting.chunkFill, help="指定拼图块背景颜色")
+@click.option("-l", "--outline", default=setting.chunkOutline, help="指定拼图块边框颜色.")
+@click.option("-o", "--output",default="output.png", help="指定文件输出路径")
+@click.option("-h", "--height", default=setting.chunkHeight, help="指定拼图块高度")
+@click.option("-w", "--width", default=setting.chunkWidth, help="指定拼图块宽度")
+@click.option("-m", "--mode", default=setting.imageMode, help="指定图片模式")
+def process(text, fp, mode, width, height,output, outline, color):
     imgSrc = Image.open(fp)               # 加载原图
     w, h = imgSrc.size                          # 原图尺寸
     # 创建拼图块的图片对象
-    chunk = Image.new(setting.imageMode, (setting.chunkWidth, setting.chunkHeight))
+    chunk = Image.new(mode, (width, height))
 
     # 创建拼图框架的图片对象
-    picFrame = Image.new(setting.imageMode, (setting.chunkWidth*w, setting.chunkHeight*h))
+    picFrame = Image.new(mode, (width*w, height*h))
 
     # 创建ImageDraw对象并绑定拼图块图片对象
-    draw = ImageDraw.Draw(chunk, setting.imageMode)
+    draw = ImageDraw.Draw(chunk, mode)
     charIndex = 0                               # 待渲染的字符在text序列里的索引
 
     for y in range(h):
         print(f"正在处理第{y+1}行, 还剩{h-y-1}行未处理.")
         for x in range(w):
             # 绘制拼图块
-            drawChar2ChunkByImageDrawObj(text[charIndex], draw, setting.chunkWidth/2, setting.chunkHeight/2, fill=imgSrc.getpixel((x,y)))
+            drawChar2ChunkByImageDrawObj(
+                text[charIndex],
+                draw,
+                width/2,
+                height/2,
+                fill=imgSrc.getpixel((x,y)),
+                color=color,
+                outline=outline,
+            )
 
             # 拼图块拼到拼图框架的合适位置 (粘贴到指定位置)
             picFrame.paste(chunk, (x*setting.chunkWidth, y*setting.chunkHeight))
